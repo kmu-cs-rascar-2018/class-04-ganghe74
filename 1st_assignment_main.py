44344754da104c392b4407e4b41a680e3f1c7872
ganghe74 <ganghe74@kookmin.ac.kr>
2018-10-08 20:57:02 +0900

    modify

--- 1st_assignment_main.py
+++ 1st_assignment_main.py
@@ -1,121 +1,35 @@
 #########################################################################
-# Date: 2018/08/09
+# Date: 2018/10/02
 # file name: 1st_assignment_main.py
 # Purpose: this code has been generated for the 4 wheels drive body
 # moving object to perform the project with ultra sensor
 # this code is used for the student only
 #########################################################################
-
-
-# =======================================================================
-# import GPIO library and time module
-# =======================================================================
-import RPi.GPIO as GPIO
+from car import Car
 import time
 
-# =======================================================================
-# import ALL method in the SEN040134 Tracking Module
-# =======================================================================
-from SEN040134 import SEN040134_Tracking as Tracking_Sensor
+class myCar(object):
 
-# =======================================================================
-# import ALL method in the TCS34725 RGB Module
-# =======================================================================
-from TCS34725 import TCS34725_RGB as RGB_Sensor
-
-# =======================================================================
-# import ALL method in the SR02 Ultrasonic Module
-# =======================================================================
-from SR02 import SR02_Ultrasonic as Ultrasonic_Sensor
-
-# =======================================================================
-# import ALL method in the rear/front Motor Module
-# =======================================================================
-import rear_wheels
-import front_wheels
-
-# =======================================================================
-#  set GPIO warnings as false
-# =======================================================================
-GPIO.setwarnings(False)
-
-
-class Car(object):
-
-    def __init__(self):
-        self.moduleInitialize()
+    def __init__(self, car_name):
+        self.car = Car(car_name)
 
     def drive_parking(self):
-        # front wheels center allignment
-        self.front_steering.turn_straight()
-
-        # power down both wheels
-        self.rear_wheels_drive.stop()
-        self.rear_wheels_drive.power_down()
+        self.car.drive_parking()
 
     # =======================================================================
     # 1ST_ASSIGNMENT_CODE
     # Complete the code to perform First Assignment
     # =======================================================================
-    def assignment_main(self):
+    def car_startup(self):
         # Implement the assignment code here.
         pass
 
-    def moduleInitialize(self):
-        try:
-            # ================================================================
-            # ULTRASONIC MODULE DRIVER INITIALIZE
-            # ================================================================
-            self.distance_detector = Ultrasonic_Sensor.Ultrasonic_Avoidance(35)
-
-            # ================================================================
-            # TRACKING MODULE DRIVER INITIALIZE
-            # ================================================================
-            self.line_detector = Tracking_Sensor.SEN040134_Tracking([16, 18, 22, 40, 32])
-
-            # ================================================================
-            # RGB MODULE DRIVER INITIALIZE
-            # ================================================================
-            self.color_getter = RGB_Sensor.TCS34725()
-
-            # ================================================================
-            # FRONT WHEEL DRIVER SETUP
-            # ================================================================
-            self.front_steering = front_wheels.Front_Wheels(db='config')
-            self.front_steering.ready()
-
-            # ================================================================
-            # REAR WHEEL DRIVER SETUP
-            # ================================================================
-            self.rear_wheels_drive = rear_wheels.Rear_Wheels(db='config')
-            self.rear_wheels_drive.ready()
-
-            # ================================================================
-            # SET LIMIT OF TURNING DEGREE
-            # ===============================================================
-            self.front_steering.turning_max = 35
-
-            # ================================================================
-            # SET FRONT WHEEL CENTOR ALLIGNMENT
-            # ================================================================
-            self.front_steering.turn_straight()
-
-            # ================================================================
-            # DISABLE RGB MODULE INTERRUPTION
-            # ================================================================
-            self.color_getter.set_interrupt(False)
-
-        except:
-            print("MODULE INITIALIZE ERROR")
-            print("CONTACT TO Kookmin Univ. Teaching Assistant")
-
-
 if __name__ == "__main__":
     try:
-        car = Car()
-        car.assignment_main()
+        myCar = myCar("CarName")
+        myCar.car_startup()
 
     except KeyboardInterrupt:
         # when the Ctrl+C key has been pressed,
         # the moving object will be stopped
-        car.drive_parking()
+        myCar.drive_parking()
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 20181653 이강희
 
-Ultrasonic : 초음파의, Supersonic : 초음속의 이므로 Ultrasonic을 사용하겠습니다.
+파일, 클래스 등 이름이 rascar 홈페이지 , github 마다 전부 다르네요.  
+[github](https://github.com/sch2307/all_new_rascar/) 기준으로 코드를 작성했습니다.
