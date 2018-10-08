03bf9b5b56c40e4cb79c546c808544c361ce1b16
김강희 <ganghe74@kookmin.ac.kr>
2018-10-08 14:33:56 +0900

    Ultrasonic

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 20181653 이강희
+
+Ultrasonic : 초음파의, Supersonic : 초음속의 이므로 Ultrasonic을 사용하겠습니다.
--- car_control_example.py
+++ car_control_example.py
@@ -1,38 +1,45 @@
-# /Example
-from SR02 import SR02_Supersonic as Supersonic_Sensor
+# https://rascar.cs.kookmin.ac.kr
+from SR02 import SR02_Ultrasonic as Ultrasonic_Sensor
 
 import front_wheels
 import rear_wheels
 import time
 
 if __name__ == '__main__':
+
     try:
+
         # Example Of Front Servo Motor Control
-        steering = front_wheels.Front_Wheels(db='config')
-        steering.center_alignment()
-        time.sleep(1)
-        
-        # Example Of Real Motor Control
-        accelerator = rear_wheels.Rear_Wheels(db='config')
-        accelerator.ready()
-        accelerator.go_forward(50)
-        time.sleep(1)
-        accelerator.stop()
-        time.sleep(1)
-        accelerator.go_backward(50)
+
+        direction_controller = front_wheels.Front_Wheels(db='config')
+
+        direction_controller.turn_straight()
         time.sleep(1)
 
-        accelerator.stop()
-        accelerator.power_down()
-        
+        # Example Of Real Motor Control
+
+        driving_controller = rear_wheels.Rear_Wheels(db='config')
+
+        driving_controller.ready()
+        driving_controller.forward_with_speed(50)
+        time.sleep(1)
+        driving_controller.stop()
+        time.sleep(1)
+        driving_controller.backward_with_speed(50)
+        time.sleep(1)
+
+        driving_controller.stop()
+        driving_controller.power_down()
+
         # Example of Ultrasonic Sensor
-        distance_detector = Supersonic_Sensor.Supersonic_Sensor(35)
+
+        distance_detector = Ultrasonic_Sensor.Ultrasonic_Avoidance(35)
 
         for i in range(10):
             distance = distance_detector.get_distance()
             print("Distance is ", distance)
             time.sleep(1)
-        
+
     except KeyboardInterrupt:
-        accelerator.stop()
-        accelerator.power_down()
+        back.stop()
+        back.power_down()
