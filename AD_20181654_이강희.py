d9f65ad56c3fdb9ea0ec2970fac34fcdcfb96191
김강희 <ganghe74@kookmin.ac.kr>
2018-12-08 11:53:15 +0900

    AD bonecode

---
+++ AD_20181654_이강희.py
@@ -0,0 +1,100 @@
+#########################################################################
+# Date: 2018/10/02
+# file name: 2nd_assignment_main.py
+# Purpose: this code has been generated for the 4 wheel drive body
+# moving object to perform the project with line detector
+# this code is used for the student only
+#########################################################################
+
+from car import Car
+import time
+
+
+class myCar(object):
+
+    def __init__(self, car_name):
+        self.car = Car(car_name)
+
+    def drive_parking(self):
+        self.car.drive_parking()
+
+    # =======================================================================
+    # 2ND_ASSIGNMENT_CODE
+    # Complete the code to perform Second Assignment
+    # =======================================================================
+    def car_startup(self):
+        SPEED = 50
+        self.car.steering.center_alignment()
+        self.car.accelerator.go_forward(SPEED)
+        
+        line_detector = self.car.line_detector
+        preLine = [0,0,0,0,0]
+        
+        count = 0
+
+        while count <= 2:
+
+            # 신호등 감지
+            rgb = self.car.color_getter.get_raw_data()
+            if rgb[0] > 700 and rgb[1] < 400 and rgb[2] < 400:
+                print("RED DETECTED!!")
+                self.car.accelerator.stop()
+
+            # 장애물 감지
+            distance = self.car.distance_detector.get_distance()
+            if 0 <= distance and distance < 30:
+                time.sleep(0.1)
+                if not self.car.distance_detector.get_distance() < 30: # 장애물을 재확인
+                    continue
+                print("Obstacle Detected")
+                self.car.steering.turn(90-35) # 좌회전
+                while line_detector.is_in_line():
+                    continue
+                while not line_detector.is_in_line():
+                    continue
+                self.car.steering.turn(90+35) # 우회전
+                while line_detector.is_in_line():
+                    continue
+                while not line_detector.is_in_line():
+                    continue
+
+            # 라인 벗어날 경우 (급커브)
+            if not line_detector.is_in_line():
+                print("Curve")
+                self.car.accelerator.stop()
+                self.car.steering.turn(90 + preLine[0] * 35 + preLine[4] * -35)
+                self.car.accelerator.go_backward(SPEED)
+                while not line_detector.is_in_line():
+                    continue
+                time.sleep(0.1)
+                self.car.accelerator.stop()
+                self.car.accelerator.go_forward(SPEED)
+
+            # 라인 트레이싱
+            line = line_detector.read_digital()
+            degree = [-20 if line[1] else -35, -5 if line[2] else -10, 0, 5 if line[2] else 10, 20 if line[3] else 35]
+            degree = [x*y for x, y in zip(line, degree)]
+            self.car.steering.turn(90 + sum(degree))
+
+            preLine = line
+
+            # 정지선 카운트
+            if [1,1,1,1,1] == line:
+                self.car.steering.center_alignment()
+                while line_detector.read_digital() == [1,1,1,1,1]:
+                    continue
+                count += 1
+                print("meet end", count)
+
+
+        self.drive_parking()
+
+if __name__ == "__main__":
+    try:
+        myCar = myCar("CarName")
+        myCar.car_startup()
+
+    except KeyboardInterrupt:
+        # when the Ctrl+C key has been pressed,
+        # the moving object will be stopped
+        myCar.drive_parking()
---
+++ minjong's buzzer sample.py
@@ -0,0 +1,75 @@
+#########################################################################
+# Date: 2018/10/02
+# file name: 1st_assignment_main.py
+# Purpose: this code has been generated for the 4 wheels drive body
+# moving object to perform the project with ultra sensor
+# this code is used for the student only
+#########################################################################
+
+from car import Car
+import RPi.GPIO as GPIO
+import time
+import threading
+
+class Buzzer() :
+    def __init__(self):
+        self.buzzer_pin = 8
+        GPIO.setmode(GPIO.BOARD)
+        self.scale = [261.6, 293.6, 329.6, 349.2, 391.9, 440.0, 493.8, 523.2]
+        GPIO.setup(self.buzzer_pin, GPIO.OUT)
+        self.list1 = [2, 1, 0, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 1, 0, 1, 2, 2, 2, 1, 1, 2, 1, 0]
+        self.list2 = [2, 1, 0, 1, 2, 2, 2, 1, 1, 1, 2, 2, 2, 2, 1, 0, 1, 2, 2, 2, 1, 1, 2, 1, 0]
+        self.speed = 0.1
+        self.len = len(self.list1)
+        self.current = 0
+        self.finish = False
+
+    def song(self):
+        try :
+            P = GPIO.PWM(self.buzzer_pin, 100)
+            P.start(5)
+            while True :
+                if self.current == self.len-1 :
+                    self.list1.extend(self.list2)
+                P.ChangeFrequency(self.scale[list[self.current]])
+                time.sleep(self.speed)
+                self.current += 1
+                if self.finish == True :
+                    break
+
+
+class myCar(object):
+    def __init__(self, car_name):
+        self.car = Car(car_name)
+        self.buzzer = Buzzer()
+
+    def drive_parking(self):
+        self.car.drive_parking()
+
+    # =======================================================================
+    # 1ST_ASSIGNMENT_CODE
+    # Complete the code to perform First Assignment
+    # =======================================================================
+    def car_startup(self):
+        t = threading.Thread(target=self.buzzer.song)
+        t.start()
+        a = 0
+        while True :
+            self.buzzer.speed += 0.3
+            time.sleep(1)
+            a += 1
+            if a == 20 :
+                break
+
+
+
+
+if __name__ == "__main__":
+    try:
+        myCar = myCar("CarName")
+        myCar.car_startup()
+
+    except KeyboardInterrupt:
+        # when the Ctrl+C key has been pressed,
+        # the moving object will be stopped
+        myCar.drive_parking()
